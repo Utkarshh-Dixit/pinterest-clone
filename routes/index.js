2c2b0441dd081b766378355ae70d43546624eabd
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2023-11-24 01:49:54 +0530

    user and post id connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users");
+const postModel = require("./posts");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/createuser', async function(req, res, next){
+  const createdUser = await userModel.create({
+    username: "Utkarsh",
+  password: "utkarsh",
+  posts: [],
+  email: "utkarshdixit.2k21@gmail.com",
+  fullName: "Utkarsh Dixit"
+});
+res.send(createdUser);
+});
+
+router.get('/alluserposts', async function(req, res){
+   let user = await userModel
+   .findOne({_id: "655fa8c68fbf7c85eb2f29e2"})
+   .populate('posts');
+   res.send(user);
+});
+
+router.get('/createpost', async function(req, res){
+  const createdPost = await postModel.create({
+    postText: "Hello, i am another post",
+    user: "655fa8c68fbf7c85eb2f29e2",
+  });
+  let user = await userModel.findOne({_id: "655fa8c68fbf7c85eb2f29e2"});
+  user.posts.push(createdPost._id);
+  await user.save();
+  res.send("done");
+});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/pinterest-clone");
+mongoose.connect("mongodb://127.0.0.1:27017/pinterestclone");
 
-const userSchema = new mongoose.Schema({
+const userSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
